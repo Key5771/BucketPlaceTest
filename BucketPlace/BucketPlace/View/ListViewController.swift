83e316f9f282ba24a59d29962f77f00bca60c574
key5771 <ksj57715@hanmail.net>
2020-12-06 23:40:28 +0900

    리스트 하단에 도달한 경우 같은 데이터 한번 더 조회하여 contentInfo 배열에 추가 portrait/landscape 모드의 전환을 viewWillTransition 함수 이용

--- BucketPlace/BucketPlace/View/ListViewController.swift
+++ BucketPlace/BucketPlace/View/ListViewController.swift
@@ -10,7 +10,7 @@ import UIKit
 class ListViewController: UIViewController {
     @IBOutlet weak var collectionView: UICollectionView!
     
-    let baseUrl = "https://s3.ap-northeast-2.amazonaws.com/bucketplace-coding-test/cards/page_1.json"
+    var baseUrl = "https://s3.ap-northeast-2.amazonaws.com/bucketplace-coding-test/cards/page_1.json"
     var contentInfo: [ListAPI] = []
     
     override func viewDidLoad() {
@@ -29,9 +29,14 @@ class ListViewController: UIViewController {
     private func setupUI() {
         collectionView.delegate = self
         collectionView.dataSource = self
+        collectionView.prefetchDataSource = self
         
         let nibName = UINib(nibName: "ListCollectionViewCell", bundle: nil)
         collectionView.register(nibName, forCellWithReuseIdentifier: "ListCollectionViewItem")
+    }
+    
+    override func viewWillTransition(to size: CGSize, with coordinator: UIViewControllerTransitionCoordinator) {
+        collectionView.collectionViewLayout.invalidateLayout()
     }
     
     @IBAction func orderAction(_ sender: Any) {
@@ -47,12 +52,14 @@ class ListViewController: UIViewController {
     }
 }
 
+// MARK: - UICollectionViewDelegate
 extension ListViewController: UICollectionViewDelegate {
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath) {
         print("indexPath: \(indexPath.row)")
     }
 }
 
+// MARK: - UICollectionViewDataSource
 extension ListViewController: UICollectionViewDataSource {
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
         return contentInfo.count
@@ -66,12 +73,8 @@ extension ListViewController: UICollectionViewDataSource {
         let infoData = contentInfo[indexPath.row]
         
         item.descriptionLabel.text = infoData.description
-        guard let urlString = infoData.image_url, let url = URL(string: urlString) else {
+        guard let urlString = infoData.image_url, let url = URL(string: urlString), let imageData = try? Data(contentsOf: url) else {
             fatalError("imageURL is nil")
-        }
-        
-        guard let imageData = try? Data(contentsOf: url) else {
-            fatalError()
         }
         item.imageView.image = UIImage(data: imageData)
         
@@ -79,8 +82,26 @@ extension ListViewController: UICollectionViewDataSource {
     }
 }
 
+// MARK: - UICollectionViewDataSourcePrefetching : 리스트 하단에 도착하면 같은 데이터 조회하여 collectionView reloadData
+extension ListViewController: UICollectionViewDataSourcePrefetching {
+    func collectionView(_ collectionView: UICollectionView, prefetchItemsAt indexPaths: [IndexPath]) {
+        dataLoad(indexpaths: indexPaths)
+    }
+    
+    private func dataLoad(indexpaths: [IndexPath]) {
+        guard let lastIndex = indexpaths.last?.item, lastIndex >= contentInfo.count - 1 else { return }
+        NetworkRequest.shared.getData(baseUrl: baseUrl) { (response: [ListAPI]) in
+            self.contentInfo.append(contentsOf: response)
+            
+            DispatchQueue.main.async {
+                self.collectionView.reloadData()
+            }
+        }
+    }
+}
+
 extension ListViewController: UICollectionViewDelegateFlowLayout {
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height / 1.5)
+        return CGSize(width: self.view.frame.size.width, height: self.view.frame.size.height / 1.3)
     }
 }
